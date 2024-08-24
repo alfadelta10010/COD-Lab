7e50b1198484be9ca48f28dba6d068e6592e173f
Chandana BN <chandanabn2904@gmail.com>
2024-08-24 05:40:09 +0000

    Week1 Programs done

---
+++ week1/week1q4.s
@@ -0,0 +1,23 @@
+.data
+a:.word 0x00012345
+b:.half 0x1234
+c:.byte 0x12
+
+.text
+ la x20,a
+ la x21,b
+ la x22,c
+ 
+ lw x11,0(x20) # loading word a to x11
+ 
+ lh x12,0(x21) #loading signed halfword b to x12
+ lhu x13,2(x21) #loading unsigned half word c to x13
+ 
+ lb x14,0(x22) #loading signed byte 
+ lbu x15,1(x22) #loading unsigned byte
+  
+ sw x11,4(x20) #storing word
+ 
+ sh x12,4(x21) #storing halfword
+
+ sb x13,2(x22) #storing byte
