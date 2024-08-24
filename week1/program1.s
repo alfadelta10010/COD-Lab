070267b0fccbb4ab78137c095e50e446ed0d0164
hanumanthrao0999 <151769081+hanumanthrao0999@users.noreply.github.com>
2024-08-25 01:09:20 +0530

    program1.s

--- week1/program1.s
+++ week1/program1.s
@@ -1,20 +1,8 @@
-# Program 1: 
-### Statement: Write an Assembly Program for addition of 2 words
-
-### Name of file:
-program1.s
-
-### Observation - Single Cycle
-- - Loads two words from memory.
-- Adds the two words and stores the result in a register.
-- Stores the result back in memory
- 
-### Register Mapping
-- x11:0x00000001
-  x12:0x12345678
-  x13:0x12345679
-
-### Data Mapping
-- 0x10000008:0x12345679
-- 0x10000004:0x12345678
-- 0x10000000:0x00000001;
+.data
+a: .word 0x00000001,0x12345678 #a is the reference for the base address, 32 bit number
+.text                                              
+la x10,a                       #load adrdress to base address of variable a into x10
+lw x11,0(x10)                  #load word ,x11 is the destination with offset 0
+lw x12,4(x10)                  #lload word ,x12 is the destnination with offset 4
+add x13,x11,x12                #adding word in x11 and x12 and storingit in x13
+sw x13,8(x10)                  #storing byte in x13
