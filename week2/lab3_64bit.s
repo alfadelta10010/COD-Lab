3453c6df34524a92ca963930032f036e096a827e
PES2UG22EC066 <PES2UG22EC066@pesu.pes.edu>
2024-08-24 09:03:14 +0000

    week2 subimition

---
+++ week2/lab3_64bit.s
@@ -0,0 +1,15 @@
+.data
+a: .word 0xffe3345e,0xfff677ff,0xfffffffe,0xffffffff
+.text
+la x10,a
+lw x1,0(x10)
+lw x2,4(x10)
+lw x3,8(x10)
+lw x4,12(x10)
+add x5,x2,x4 #lsb
+sltu x6,x5,x4  #lsb carry  
+add x7,x1,x3  #msb
+sltu x11,x7,x3  #msb carry
+add x8,x6,x7 #adding lsb carry to msb 
+sw x8,16(x10)
+sw x5,20(x10)
