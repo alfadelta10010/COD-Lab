78ca412f2474e0658e8ad8e2090c4357cffae068
hanumanthrao0999 <151769081+hanumanthrao0999@users.noreply.github.com>
2024-08-25 00:42:58 +0530

    program2.s

---
+++ week1/program2.s
@@ -0,0 +1,21 @@
+# Program 2: 
+ ### Statement: Write an Assembly Program for addition of 2 half words
+
+### Name of file:
+program2.s
+
+### Observation - Single Cycle
+ Single Cycle
+- Loads two half words from memory.
+- Adds the two half words and stores the result in a register.
+- Stores the result back in memory.
+
+ 
+### Register Mapping
+- x11:0x00000001
+- x12:0x00001234
+- x13:0x00001235
+
+### Data Mapping
+- 0x10000004:0x00001235
+- 0x10000001:0x12340001;
