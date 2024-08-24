9c7bb7b19022cfde1dce418318db8f6d97bff895
Adithya Bijoy <adibijoy1307@gmail.com>
2024-08-24 13:15:04 +0000

    This is my week2 submission

--- week2/adding_2_doubleword.s
+++ week2/adding_2_doubleword.s
@@ -2,14 +2,14 @@
 a: .dword, 0xFFFFFFFFFFFFFFFF,0xFFFFFFFFFFFFFFFF,0x0000000000000000
 .text
 la x10,a
-lw x11,0(x10)
-lw x12,4(x10)
-lw x13,8(x10)
-lw x14,12(x10)
-add x15,x11,x13
-sltu x20,x15,x11
-add x16,x12,x14
-sltu x21,x16,x12
-add x17,x16,x20
+lw x11,0(x10) #loading last 32 bit of first number
+lw x12,4(x10) #loading first 32 bit of first number
+lw x13,8(x10) #loading last 32 bit of second number
+lw x14,12(x10) #loading first 32 bit of second number
+add x15,x11,x13 #adding the 2 last 32 bits
+sltu x20,x15,x11 #checking for carry
+add x16,x12,x14 #adding the 2 first 32 bits
+sltu x21,x16,x12 #checking for carry
+add x17,x16,x20 #adding carry of sum of last 32 bits to sum of first 32 bits
 sw x15,16(x10)
 sw x17,20(x10)
--- week2/adding_2_pointers(no loop).s
+++ week2/adding_2_pointers(no loop).s
@@ -3,26 +3,26 @@ a: .word,0x12,0x11,0x24,0x34,0x56
 b: .word,0x23,0x22,0x35,0x45,0x67
 c: .word,0,0,0,0,0
 .text
-la x10,a
-la x11,b
-la x12,c
-lw x13,0(x10)
-lw x14,4(x10)
-lw x15,8(x10)
-lw x16,12(x10)
-lw x17,16(x10)
-lw x18,0(x11)
-lw x19,4(x11)
-lw x20,8(x11)
-lw x21,12(x11)
-lw x22,16(x11)
-add x23,x13,x18
-add x24,x14,x19
-add x25,x15,x20
-add x26,x16,x21
-add x27,x17,x22
-sw,x23,0(x12)
-sw,x24,4(x12)
-sw,x25,8(x12)
-sw,x26,12(x12)
-sw,x27,16(x12)
+la x10,a #loading a to x10
+la x11,b #loading b to x11
+la x12,c #loading c to x12
+lw x13,0(x10) 
+lw x14,0(x11)
+add x15,x13,x14 
+sw x15,0(x12)
+lw x13,4(x10)
+lw x14,4(x11)
+add x15,x13,x14
+sw x15,4(x12)
+lw x13,8(x10)
+lw x14,8(x11)
+add x15,x13,x14
+sw x15,8(x12)
+lw x13,12(x10)
+lw x14,12(x11)
+add x15,x13,x14
+sw x15,12(x12)
+lw x13,16(x10)
+lw x14,16(x11)
+add x15,x13,x14
+sw x15,16(x12)
