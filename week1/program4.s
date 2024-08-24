3aa5d20fd0d0ac72445490f743c027ce864469a4
hanumanthrao0999 <151769081+hanumanthrao0999@users.noreply.github.com>
2024-08-25 00:44:45 +0530

    program4.s

---
+++ week1/program4.s
@@ -0,0 +1,20 @@
+# Program 4: 
+### Statement:  Write an Assembly Program and analyze the format of storing signed and unsigned words, half words, and byte data types
+
+
+### Name of file:
+program4.s
+
+### Observation - Single Cycle
+- Loads signed words, half-words, and bytes from memory.
+- Adds them and stores the results in memory.
+
+
+### Register Mapping
+ x11:0x00000001
+ x12:0x00001234
+ x13:0x00001235
+
+### Data Mapping
+- 0x10000004:0x00001235
+- 0x10000000:0x12340001;
