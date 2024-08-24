115494779b256d7b71d174779f377f75f6a4760a
Khushi Tikkiwal <73636733+khusxii@users.noreply.github.com>
2024-08-24 21:12:28 +0530

    Update week1q4.s

--- week1/week1q4.s
+++ week1/week1q4.s
@@ -2,46 +2,46 @@
 .data
 a: .byte 0xFF
 .text
-la x10, a
-lb x11, 0(x10)
-sb x11, 1(x10)
+la x10, a #loading base address
+lb x11, 0(x10) #loading value into register
+sb x11, 1(x10) #storing in memory
 
 #unsigned byte
 .data
 a: .byte 0xFF
 .text
-la x10, a
-lbu x11, 0(x10)
-sb x11, 1(x10)
+la x10, a #loading base address
+lbu x11, 0(x10) #loading value into register
+sb x11, 1(x10) #storing in memory
 
 #signed half word
 .data
 a: .half 0xFFFF
 .text
-la x10, a
-lh x11, 0(x10)
-sh x11, 1(x10)
+la x10, a #loading base address
+lh x11, 0(x10) #loading value into register
+sh x11, 1(x10) #storing in memory
 
 #unsigned half word
 .data
 a: .half 0xFFFF
 .text
-la x10, a
-lhu x11, 0(x10)
-sh x11, 1(x10)
+la x10, a #loading base address
+lhu x11, 0(x10) #loading value into register
+sh x11, 1(x10) #storing in memory
 
 #signed word
 .data
 a: .word 0xFFFFFFFF
 .text
-la x10, a
-lw x11, 0(x10)
-sw x11, 1(x10)
+la x10, a #loading base address
+lw x11, 0(x10) #loading value into register
+sw x11, 1(x10) #storing in memory
 
 #unsigned word
 .data
 a: .word 0xFFFFFFFF
 .text
-la x10, a
-lwu x11, 0(x10)
-sw x11, 1(x10)
+la x10, a #loading base address
+lwu x11, 0(x10) #loading value into register
+sw x11, 1(x10) #storing in memory
