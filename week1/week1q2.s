929c4f9d3ec413109b268e8750b7faead3ce6140
Khushi Tikkiwal <73636733+khusxii@users.noreply.github.com>
2024-08-24 21:08:04 +0530

    Update week1q2.s

--- week1/week1q2.s
+++ week1/week1q2.s
@@ -1,8 +1,8 @@
 .data
 a: .half 0x0002, 0x0432
 .text
-la x10, a
-lh x11, 0(x10)
-lh x12, 2(x10)
-add x13, x11, x12
-sh x13, 4(x10)
+la x10, a #loading base address
+lh x11, 0(x10) #loading value into register
+lh x12, 2(x10) #loading value into register
+add x13, x11, x12 #sum of values to be stored in x13
+sh x13, 4(x10) #storing in memory
