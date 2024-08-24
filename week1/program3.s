6cc79cfb9686626e2c887dd2122d32c0056d1905
hanumanthrao0999 <151769081+hanumanthrao0999@users.noreply.github.com>
2024-08-25 00:44:08 +0530

    program3.s

---
+++ week1/program3.s
@@ -0,0 +1,20 @@
+# Program 3: 
+### Statement:  Write an Assembly Program for addition of 2 bytes
+
+### Name of file:
+program3.s
+### Observation - Single Cycle
+- Loads two bytes from memory.
+- Adds the two bytes and stores the result in a register.
+- Stores the result back in memory.
+
+
+ 
+### Register Mapping
+- x11:0x00000001
+- x12:0x00000012
+- x13:0x00000013
+
+### Data Mapping
+- 0x10000004:0x00000000
+- 0x10000000:0x00131201;
