d8e560bc92fe00ef90b22d75ecfa357cabc890be
Khushi Tikkiwal <73636733+khusxii@users.noreply.github.com>
2024-08-24 20:55:34 +0530

    Update week2q1.s

--- week2/week2q1.s
+++ week2/week2q1.s
@@ -2,15 +2,15 @@
 a: .dword 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF
 .text
 la x10, a
-lw x11, 0(x10)
-lw x12, 4(x10)
-lw x13, 8(x10)
-lw x14, 12(x10)
-add x15, x11, x13
-add x16, x12, x14
-sltu x17, x15, x13
-sltu x18, x16, x14
-add x19, x16, x17
+lw x11, 0(x10) #LSB 1
+lw x12, 4(x10) #MSB 1
+lw x13, 8(x10) #LSB 2
+lw x14, 12(x10) #MSB 2
+add x15, x11, x13 #sum of LSB 1 and 2
+add x16, x12, x14 #sum of MSB 1 and 2
+sltu x17, x15, x13 #storing LSB carry
+sltu x18, x16, x14 #storing MSB carry
+add x19, x16, x17 #adding LSB carry to MSB sum
 sw x20, 16(x15)
 sw x21, 20(x19)
 sw x22, 24(x18)
