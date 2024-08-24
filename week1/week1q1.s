0508d20c04bafd5350a2aaaff4a8cf226696761a
Khushi Tikkiwal <73636733+khusxii@users.noreply.github.com>
2024-08-24 21:06:15 +0530

    Update week1q1.s

--- week1/week1q1.s
+++ week1/week1q1.s
@@ -1,8 +1,8 @@
 .data
 a: .word 0x80000002, 0x9123456A
 .text
-la x10, a
-lw x11, 0(x10)
-lw x12, 4(x10)
-add x13, x11, x12
-sw x13, 8(x10)
+la x10, a #loading base address
+lw x11, 0(x10) #loading values in register
+lw x12, 4(x10) #loading values in register
+add x13, x11, x12 #sum of the values into x13
+sw x13, 8(x10) #storing in memory
