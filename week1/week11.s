09f878b56d5624ace72e81efb01a42e83c10a37f
Nagesh Basanagoud Patil <nagupatil9900@gmail.com>
2024-08-24 18:42:00 +0530

    Added Week1 Files

---
+++ week1/week11.s
@@ -0,0 +1,8 @@
+.data
+a: .word 0x40001111,0x10000080
+
+.text
+la x10,a #loading the base address
+lw s0,0(x10) #loading the content of data memory into register
+lw s1,4(x10) #same for next data
+add s3,s0,s1 #adding the two data
---
+++ week1/week12.s
@@ -0,0 +1,7 @@
+.data
+a: .half 0x4000,0x2000 #this will the data memory which contains our two data
+.text
+la x10,a #loading the base address
+lh s0,0(x10) #loading the first data into x10
+lh s1,2(x10) #loding the second one
+add s2,s0,s1 #adding the two half word data
