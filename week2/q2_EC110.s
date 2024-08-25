24fffeb9652fbe74a10afe11624ee0c255c330f9
Rohan <rohannr1711@gmail.com>
2024-08-25 11:45:58 +0530

    Submission

--- week2/q2_EC110.s
+++ week2/q2_EC110.s
@@ -4,12 +4,12 @@ n2: .dword 0xffffffffffffffff
 .text
 la x10,n1
 la x11,n2
-lw x12,0(x10) #loading
+lw x12,0(x10) 
 lw x13,4(x10)
 lw x14,0(x11)
 lw x15,4(x11)
-add x16,x14,x12 #lsb
+add x16,x14,x12 
 sltu x18,x16,x14
-add x17,x15,x13 #msb
+add x17,x15,x13 
 sltu x19,x17,x15
-add x20,x17,x18 #adding
+add x20,x17,x18 
