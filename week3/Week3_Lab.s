65fa73a1467879a42442e4cf518559ed8329f5de
Saanvi <saanvisinha14@gmail.com>
2024-09-05 14:51:01 +0000

    Week 3 assignment done TA - Jitesh

---
+++ week3/Week3_Lab.s
@@ -0,0 +1,20 @@
+#Program 1:Convert a 32-bit value from Little Endian to Big Endian format using RISC-V assembly
+
+.data
+a: .word 0x12345678
+.text
+la x10,a
+lw x11,0(x10)
+addi x3,x0,3
+
+back:
+    andi x12,x11,0xFF
+    add x14,x14,x12
+    slli x14,x14,8
+    srli x11,x11,8
+    addi x3,x3,-1
+bnez x3,back
+    andi x15,x11,0xFF
+    add x15,x14,x15
+    sw x15,8(x10)
+
