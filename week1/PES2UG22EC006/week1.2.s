bb8fb4a7452e849c60f9c5c228b8bf42ed9f7c95
Abhi-244 <abhigowda592004@gmail.com>
2024-09-10 14:35:44 +0530

    Delete week1/PES2UG22EC006/week1.2.s

--- week1/PES2UG22EC006/week1.2.s
+++
@@ -1,9 +0,0 @@
-.data                                     
-n: .half 0x1234, 0x1212     # Define two 16-bit half-words at label 'n'
-.text 
-la x10,n                     # Load address of 'n' into x10                          
-lh x5,0(x10)                  # Load the first half-word (0x1234) into x5                        
- lh x6,2(x10)                   # Load the second half-word (0x1212) into x6     
-add x7,x5,x6                     # Add x5 and x6, store result in x7  
-sh x7,4(x10)                            
-                           
