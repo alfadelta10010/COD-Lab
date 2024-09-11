ed50cbfd79b38152136703e331dce9e257104fc4
SRINIVAS THATTE <vasut040504@gmail.com>
2024-09-11 16:24:46 +0000

    WEEK3 DONE

--- week1/programs-138/Wordcode.s
+++ week1/programs-138/Wordcode.s
@@ -1,9 +1,28 @@
 .data 
-a: .word 0x54e3,0x0104,0
+b: .byte 0x12
 
 .text
-la x10,a
-lw x7,0(x10)
-lw x8,4(x10)
-add x11,x7,x8
-sw x11,8(x10)
+la x10,b
+lb x11,0(x10)
+addi x20,x0,1 #i=1
+addi x21,x0,5 # h=5
+addi x22,x0,0 #count=0
+addi x23,x0,2 # 2
+addi x12,x11,0 #x12=x11
+andi x13,x11,0xe0
+bne x13,x0,zxc
+
+
+zxc:
+    and x15,x12,x20
+    bne x15,x0,inc
+    addi x22,x22,1
+    beq x21,x0,exit
+    
+inc:
+    addi x21,x21,-1 #h--
+    srli x12,x12,0
+    bne x22,x23,zxc
+    
+    
+exit:nop
