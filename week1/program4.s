868bdad5b6179691a89b44362fa7f78f8c2853a1
Prajwal Pavana Gunda <prajwalpavana13@gmail.com>
2024-09-12 03:39:33 +0000

    x

--- week1/program4.s
+++ week1/program4.s
@@ -1,18 +1,28 @@
 .data
-w: .word 0x12345678
-h: .half 0x1234
-b: .byte 0xff
+a: .word 0x01,0x02,0x03,0x04,0x05
+b: .word 0x10,0x20,0x30,0x40,0x50
+c: .word 0x00,0x00,0x00,0x00,0x00,
 .text
-la x10,w
-la x11,h
-la x12,b
+la x10,a
+la x11,b
+la x12,c
 lw x13,0(x10)
-lh x14,0(x11)
-lhu x15,0(x11)
-lb x16,0(x12)
-lbu x17,0(x12)
-sw x13,4(x10)
-sw x14,2(x10)
-sw x15,4(x10)
-sw x16,1(x10)
-sw x17,2(x10)
+lw x14,4(x10)
+lw x15,8(x10)
+lw x16,12(x10)
+lw x17,16(x10)
+lw x18,0(x11)
+lw x19,4(x11)
+lw x20,8(x11)
+lw x21,12(x11)
+lw x22,16(x11)
+add x23,x13,x18
+add x24,x14,x19
+add x25,x15,x20
+add x26,x16,x21
+add x27,x17,x22
+sw,x23,0(x12)
+sw,x24,4(x12)
+sw,x25,8(x12)
+sw,x26,12(x12)
+sw,x27,16(x12)
