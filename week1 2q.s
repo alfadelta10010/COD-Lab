d66d2d1fcdb61e0263389b5da6124d9c591d4e52
Puneeth-2606 <pes2ug23ec802@pesu.pes.edu>
2024-10-26 20:11:05 +0530

    Delete week1 2q.s

--- week1 2q.s
+++
@@ -1,9 +0,0 @@
-.data
-a: .half 0x6641,0x0008
-
-.text
-la x10,a ##storindg a address in x10
-lh x11,0(x10) ##word form in memory register as bytes(0 to 1st position)
-lh x12,2(x10) ##word form in memory register as bytes(2nd to 3rd position)
-add x13,x11,x12 ##adding of x11 and x12
-sh x13 4(x10) ##storing added number in memory
