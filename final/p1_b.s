ec79cfea0cba275e8d75c37dad09c4fad3c10f91
Pranav Varkey <ptv1324ptv@outlook.com>
2024-11-13 11:27:42 +0530

    Added programs
    1, 3a, 6

---
+++ final/p1_b.s
@@ -0,0 +1,50 @@
+.data
+a:    .byte 0x0
+
+.text
+
+# Making the array :(Forgive my brain)
+la x10, a
+addi x20, x20, 3 # register
+addi x21, x21, 16 # register
+addi x22, x22, 0xde
+addi x23, x23, 0xf0
+
+rst:
+	addi x11, x0, 0x12
+	j make_loop
+last:
+	slli x11, x11, 8
+	addi x11, x11, 0xf0
+store:
+	andi x12, x11, 0xff
+	beq x12, x22, last
+	sw x11, 0(x10)
+	addi x10, x10, 4
+	addi x20, x20, 3
+	beq x23, x12, rst
+make_loop:
+	andi x12, x11, 0xff
+	addi x12, x12, 0x22
+	slli x11, x11, 8
+	add x11, x11, x12
+
+	addi x20, x20, -1 # increment
+	addi x21, x21, -1 # increment
+	beqz x20, store
+	bgez x21, make_loop
+
+# Adding the array  :
+    
+la x10, a
+add x11, x0, x0
+add x12, x0, x0
+addi x20, x0, 10
+add_loop:
+	lhu x12, 0(x10)
+	add x11, x11, x12
+
+	addi x10, x10, 2
+	addi x20, x20, -1
+	bnez x20, add_loop
+exit:
---
+++ final/p6.s
@@ -0,0 +1,34 @@
+
+.data 
+a: .byte 0x0e, 0x0d, 0x10, 0x02, 0x07, 0x04
+
+.text
+addi x21, x0, 5 # final index or number of elements
+
+la x10, a # Initial address
+add x20, x10, x21 # final address
+
+loop1: # outer loop
+    loop2: # inner loop
+        lbu x11, 0(x10) # Get element
+        lbu x12, 1(x10) # Get next element
+        bgt x11, x12, swap
+		addi x10, x10 ,1 # Goto next element
+		bne x10, x20, loop2 # Loop until it reaches final address
+
+	addi x20, x20, -1 # reduce cycle count by not check the last elements
+
+    la x10, a # Reset address or go back to beginning of array
+    addi x1, x1, 1 # Outer counter
+    bne x1, x21, loop1 # Loop until all indexes covered
+    j exit
+
+swap:
+    sb x11, 1(x10)
+    sb x12, 0(x10)
+    j loop2
+
+exit:
+    addi a0, x0, 0 # exit code
+    addi a7, x0, 93
+    ecall
