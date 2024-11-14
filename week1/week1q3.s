9959bc0cf4104934479f046cfeb444c76686b1a8
Khushi Tikkiwal <73636733+khusxii@users.noreply.github.com>
2024-11-14 15:40:13 +0530

    Delete week1/week1q3.s

--- week1/week1q3.s
+++
@@ -1,8 +0,0 @@
-.data
-a: .byte 0x12, 0x32
-.text
-la x10, a #loading base address
-lb x11, 0(x10) #loading value into register
-lb x12, 1(x10) #loading value into register
-add x13, x11, x12 #sum of values to be stored in x13
-sb x13, 2(x10) #storing in memory
