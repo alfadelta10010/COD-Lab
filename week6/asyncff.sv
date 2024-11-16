952ab59978340ea106743cbe6d80845430afbeee
Brinda Chauhan <113535398+brindach@users.noreply.github.com>
2024-11-16 22:43:24 +0530

    Create asyncff.sv

---
+++ week6/asyncff.sv
@@ -0,0 +1,16 @@
+module async_reset_flipflop(
+    input logic clk,     
+    input logic reset,    
+    input logic d,       
+    output logic q     
+);
+
+    always_ff @(posedge clk or posedge reset) 
+begin
+        if (reset)      ]
+            q <= 0;
+        else             
+            q <= d;
+    end
+
+endmodule
