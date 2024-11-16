6350a5d25c4bb7628d6cbf8049a61a09d42cf9c2
Keerthana Immanuel <113651766+keerthanaimmanuel@users.noreply.github.com>
2024-11-17 00:01:15 +0530

    Add files via upload

---
+++ week9/wb.sv
@@ -0,0 +1,11 @@
+module wb #(parameter n=32)(   
+    input logic [n-1:0] alu_out,     // ALU output  
+    input logic [n-1:0] data_in,     // Data from memory  
+    input logic mem_reg,             // Memory-to-register control signal  
+    output logic [n-1:0] wr_data     // Write-back data  
+);  
+
+    // MUX to select between ALU result and data from memory  
+    assign wr_data = mem_reg ? data_in : alu_out;  
+
+endmodule  
