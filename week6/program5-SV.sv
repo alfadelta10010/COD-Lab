d83c9ebc4c6fdcdf85bca059e0bf3e1e3e23fd3f
adityamaniraja <adityamaniraja35@gmail.com>
2024-11-17 16:38:35 +0000

    Week6, Program5SV

---
+++ week6/program5-SV.sv
@@ -0,0 +1,8 @@
+module add #(parameter N=32) (a,b,cin,sum,cout);
+  input logic [N-1:0]a;
+  input logic [N-1:0]b;
+  input logic cin;
+  output logic cout;
+  output logic [N-1:0]sum;
+  assign {cout,sum}=a+b+cin;
+endmodule
