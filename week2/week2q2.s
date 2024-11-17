d012da97d0bb51a393ff637bf8d86ab8c2c4b1f5
Vijit R <104123892+Vijit30@users.noreply.github.com>
2024-11-17 23:50:30 +0530

    Update week2q2.s

--- week2/week2q2.s
+++ week2/week2q2.s
@@ -1,9 +1,21 @@
-#PROGRAM 2. Write an Assembly Program for addition of 2 64-bit numbers on RV32I 
-.data
-b: .word 0xFEABCFDE,0xFFEEACDB,0xEEEEAAAA,0xBBBBCCCC,0
+## ADDING 2 64BIT VALUES USING RV32I
+.data 
+n1: .dword 0XFFFFFFFFFFFFFFFF    
+n2: .dword 0XFFFFFFFFFFFFFFFF    
 .text
-la x10,b
-lw x11,0(x10)
-	@@ -64,3 +53,4 @@ add x16,x15,x14
-add x17,x16,x18
-sltu x19,x17,x16 #to check for carry
+
+la x5,n1
+la x6,n2
+
+lw x7,0(x5) #lower 32 bits of n1
+lw x8,4(x5)#upper 32 bits of n1
+
+lw x9,0(x6)#lower 32 bits of n2
+lw x10,4(x6)#upper 32 bits of n2
+
+add x11,x9,x7
+sltu x13,x11,x7
+add x12,x8,x10
+sltu x14,x12,x10
+
+add x15,x13,x14
