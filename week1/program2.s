3159e3f01d71cbb489f0882025f0e86c1d6841c1
AnandKadankot <anandkadankot@gmail.com>
2024-11-18 00:38:28 +0530

    program2.s

---
+++ week1/program2.s
@@ -0,0 +1,15 @@
+.data
+num1: .dword 0xffffffffffffffff
+num2: .dword 0xffffffffffffffff
+.text
+la x10,num1
+la x11,num2
+lw x12,0(x10) #loading
+lw x13,4(x10)
+lw x14,0(x11)
+lw x15,4(x11)
+add x16,x14,x12 #lsb
+sltu x18,x16,x14
+add x17,x15,x13 #msb
+sltu x19,x17,x15
+add x20,x17,x18 #adding
