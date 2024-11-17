c785f7e706a7afa5cb34f3ef4496a008cd218e0b
Shrikrishna Pandit <panditshrikrishna10>
2024-11-17 12:20:29 +0000

    last few things

---
+++ week9/p9_d.s
@@ -0,0 +1,8 @@
+.data 
+x: .word 0x012345678
+.text
+auipc x10,0x10000
+addi x10 x10 0    #All Hazards are resolved by the Hazard Unit by introducing nops (Stalls)
+lb x5,0(x10)	  # Dependencies are resolved by forwarding unit
+addi x7,x5,0x56
+add x9,x5,x7
