f66ebed3fcdf81181380999fd615bfd438d7741d
rohannr1711 <rohannr1711@gmail.com>
2024-11-17 22:23:41 +0530

    Add files via upload

---
+++ week9/Memory_access.sv
@@ -0,0 +1,10 @@
+module MemoryAcess(clk,reg2,result, MemRead, MemWrite,dataR,zero,Branch,b_sel);
+input bit clk;
+input logic [31:0] reg2,result;
+input logic  MemRead, MemWrite;
+output logic [31:0] dataR;
+input logic zero,Branch;
+output logic b_sel;
+DataMem mem(.clk(clk),.dataW(reg2),.addr(result),.memR(MemRead),.memW(MemWrite),.dataR(dataR));
+AND_gate anding(.a(Branch),.b(zero),.y(b_sel));
+endmodule
