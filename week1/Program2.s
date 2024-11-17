89438160a07d15d8b048e604b1ae8387af51bc12
abbhi28 <narmadha993@gmail.com>
2024-11-17 15:08:06 +0530

    Week1 Uploaded

--- week1/Program2.s
+++ week1/Program2.s
@@ -1,8 +1,16 @@
 .data
-a: .half 0x0001,0x1234
+a: .dword 0xffffffffffffffff
+b: .dword 0xffffffffffffffff
 .text
-la x10,a
-lh x11,0(x10)
-lh x12,2(x10)
-add x13,x11,x12
-sh x13,4(x10)
+la x10, a
+la x11, b
+lw x12,0(x10)
+lw x13,4(x10)
+lw x14,0(x11)
+lw x15,4(x11)
+add x16,x12,x14
+sltu x17,x16,x14
+add x18,x13,x15
+sltu x19,x18,x13
+add x20,x18,x17
+
