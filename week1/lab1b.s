04f8abbf77845f197d5c7caea5c2ee117b7a06ed
eshapangarkar <115869563+eshapangarkar@users.noreply.github.com>
2024-11-17 15:49:32 +0530

    Delete week1/lab1b.s

--- week1/lab1b.s
+++
@@ -1,8 +0,0 @@
-.data
-a: .half 0x8002,0x9126
-.text
-la x10,a
-lh x11,0(x10) #half load of number 1
-lh x12,2(x10) #half load iof number 2
-add x13,x11,x12 #add the 2 numbers
-sh x13,4(x10) #store the half words addition
