774e5be2d44fee78e07a062cff5cd2de4e94b28d
Your Name <themanth707@gmail.com>
2024-11-18 09:18:37 +0530

    fixed error

--- week5/w5p2.s
+++ week5/w5p2.s
@@ -1,24 +1,31 @@
-.data # array equation
-a: .byte 0x11, 0x22, 0x33, 0x44, 0x11
-b: .byte 0x11, 0x22, 0x33, 0x44, 0x11
-c: .byte 0x11, 0x22, 0x33, 0x44, 0x11
+.data
+a: .half 0x1234, 0x5678
+b: .half 0x1234, 0x5678
+c: .half 0x1234, 0x5678
 
 .text
-la x10, a # starting index
-la x11, b # starting index
-la x12, c # starting index
-addi x15, x10, 10 # ending index
-sw x0, -1(x12) # Initialisation of c[-1]
+
+main:
+    la x10, a              # Load base address of array a into x10
+    la x11, b              # Load base address of array b into x11
+    la x12, c              # Load base address of array c into x12
+
+    addi x13, x0, 10       # Load loop count (10 iterations) into x13
+    addi x14, x0, 1        # Set loop counter to 1 (i = 1)
 
 loop:
-    lb x20, 0(x10)
-    lb x21, 0(x11)
-    lb x22, -1(x12)
-    
-    mul x23, x20, x21
-    add x24, x23, x22
-    sw x22, 0(x24)
-    
-    addi x10, x10, 1
-    addi x11, x11, 1
-    bne x10, x15, loop
+    bge x14, x13, exit     
+    lhu x15, 0(x10)         # Load elements a[i]
+    lhu x16, 0(x11)         # Load elements b[i]
+    mul x17, x15, x16      # x17 = a[i] * b[i]
+    lhu x18, -2(x12)        # Load c[i-1]
+    add x19, x17, x18      # x19 = x17 + x18
+    sh x19, 0(x12)        
+    addi x10, x10, 2       # Move to next element in array a
+    addi x11, x11, 2       # Move to next element in array b
+    addi x12, x12, 2       # Move to next element in array c
+    addi x14, x14, 1       
+    j loop               
+
+exit:
+    addi x30,x0,0
