159effdb727b1285f1a73284cfd1b30ea41585bf
A C ANIRUDH RAJ <root@AC-ANIRUDH.>
2024-11-18 08:22:24 +0000

    Adding week6

---
+++ week6/Program2A_rec.s
@@ -0,0 +1,24 @@
+#WAP to calculate the factorial of a number with & without recursion
+
+#  with recursion
+
+addi x10, x0, 5
+addi x11, sp, 0
+addi x12, x0, 1
+#addi x13, x0, 1
+
+push: # push all required values into stack
+    sw x10, 0(sp)
+    addi sp, sp, 4
+    addi x10, x10, -1
+    bne x10, x12, push
+    
+    sw x10, 0(sp)
+
+pop: # pop elements and multiply
+    addi sp, sp, -4
+    lw x10, 0(sp)
+    mul x12, x12, x10
+    bne sp, x11, pop
+
+
---
+++ week6/program1.s
@@ -0,0 +1,32 @@
+# WAP FOR bubble sort
+.data
+a: .word 8,7,6,5,4,3,2,1
+.text
+
+la x10, a
+addi x11, x0, 8    # amount of iterations for the first loop
+addi x12, x0, 8
+
+loop:
+    addi x11, x11, -1
+    ble x11, x0, iter
+    lw x5, 0(x10)
+    lw x6, 4(x10)
+    addi x10, x10, 4
+    bltu x6, x5, swap
+    bgeu x6, x5, loop
+    
+swap:
+    sw x5, 0(x10)
+    sw x6, -4(x10)
+    j loop
+    
+iter:
+    addi x12, x12, -1
+    ble x12, x0, exit
+    la x10, a
+    addi x11, x12, 0
+    j loop
+    
+exit:
+    nop
