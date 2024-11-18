c70676f0ca0cceaa24da2007be18c28ec13ddbcf
sachint19004 <sachint19122004@gmail.com>
2024-11-18 03:04:11 +0000

    assignment

--- week1/64 bit addition.s
+++ week1/64 bit addition.s
@@ -1,19 +1,21 @@
 .data
-n:.dword 0x9379316688748371, 0x8660282002    # Define 'n' as a double-word array with two values
-result:.dword 0, 0                            # Define 'result' as a double-word array initialized to 0
+n:      .word 0x93793166, 0x88748371, 0x86602820, 0x00000002  # Split original 64-bit values
+result: .word 0, 0, 0                # Define space for 3 results (32-bit each)
 
 .text
-la x4,n   # Load the address of 'n' into register x4
-lw x10,0(x4) # Load the first 8-byte (64-bit) value from 'n' into register x10
-lw x11,4(x4)  # Load the second 8-byte value from 'n' into register x11
-lw x12,8(x4)  # Load the third 8-byte value from 'n' into register x12 (however, this value doesn't exist in 'n')
-lw x13,12(x4) # Load the fourth 8-byte value from 'n' into register x13 (also does not exist in 'n', it should be out of bounds)
-add x14,x10,x12  # Add the values in x10 and x12 and store the result in x14
-sltu x15,x14,x10  # Set x15 to 1 if x14 (sum) is less than x10 (unsigned comparison), otherwise set x15 to 0
-add x16,x11,x13   # Add the values in x11 and x13 and store the result in x16
-sltu x17,x16,x11  # Set x17 to 1 if x16 (sum) is less than x11 (unsigned comparison), otherwise set x17 to 0
-add x16,x16,x15   # Add the value in x15 to x16, modifying x16 based on the previous comparison
-la x21,result     # Load the address of 'result' into register x21
-sw x14,0(x21)     # Store the value in x14 (sum of x10 and x12) at the first position in 'result'
-sw x16,4(x21)     # Store the value in x16 (sum of x11 and x13 plus comparison result) at the second position in 'result'
-sw x17,8(x21)     # Store the value in x17(comparison result) at the third position in 'result'
+la x4, n           # Load the address of 'n' into x4
+lw x10, 0(x4)      # Load first 32-bit word into x10 (lower part of first number)
+lw x11, 4(x4)      # Load second 32-bit word into x11 (higher part of first number)
+lw x12, 8(x4)      # Load third 32-bit word into x12 (lower part of second number)
+lw x13, 12(x4)     # Load fourth 32-bit word into x13 (higher part of second number)
+
+# 32-bit addition
+add x14, x10, x12  # Add lower parts
+sltu x15, x14, x10 # Check for carry (unsigned comparison)
+add x16, x11, x13  # Add higher parts
+add x16, x16, x15  # Add carry to higher parts
+
+# Store results
+la x21, result     # Load address of 'result'
+sw x14, 0(x21)     # Store lower part of the result
+sw x16, 4(x21)     # Store higher part of the result
